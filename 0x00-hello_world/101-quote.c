217cb8c84b1ecf0819ecc1951f42e67778992828
Emmy-05 <emekaokafor0715@gmail.com>
2022-09-08 16:44:56 -0700

    My first C task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <unistd.h>
 
 /**
- * main - Print quote
- *
- * Description: Prints Dora's quote
+ * main - Prints a text
  *
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
