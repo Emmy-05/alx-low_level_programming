13edcfe46a33fc193e5ad989a23a91cd5600e8db
Emmy-05 <emekaokafor0715@gmail.com>
2022-09-08 14:55:07 -0700

    My first C task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry piont
+ * main - Print the sizeof many var types
  *
  * Return: Always 0 (Success)
  */
@@ -13,11 +13,11 @@ int main(void)
 	char d;
 	float f;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(d));
-	printf("size of a int: %ld byte(s)\n", sizeof(a));
-	printf("size of a long int: %ld byte(s)\n", sizeof(b));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a int: %ld byte(s)\n", sizeof(a));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a char: %ld byte(s)\n", sizeof(d)); 
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
 
