83f98bba9f8b03b07e0569d8ce6f462da12f4127
Emmy-05 <emekaokafor0715@gmail.com>
2022-09-08 15:22:40 -0700

    My first C task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float;
 
-	printf("Size of a int: %ld byte(s)\n", sizeof(a));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(c));
-	printf("Size of a char: %ld byte(s)\n", sizeof(d)); 
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli)); 
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
