2aa99f749f2057bb031906c07e4ab1d95bce3c30
Emmy-05 <emekaokafor0715@gmail.com>
2022-09-08 17:21:56 -0700

    My first C task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a text
+ * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 61);
 	return (1);
 }
