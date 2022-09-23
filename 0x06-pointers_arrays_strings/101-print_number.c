ab67278dd1eba71d9ff20fd0b071bc19178ef654
Emmy-05 <emekaokafor0715@gmail.com>
2022-09-23 09:45:47 -0700

    My sixth C task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*print_number - prints an integer.
-*
-*@n: integer
-*
-*Return: void
-*/
+ *print_number - prints an integer.
+ *
+ *@n: integer
+ *
+ *Return: void
+ */
 
 void print_number(int n)
 {
