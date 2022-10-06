723b58fea1b5774b7e755d53eed7ee176cc9bf4b
Emmy-05 <emekaokafor0715@gmail.com>
2022-10-06 09:11:06 -0700

    My 0x0C. C - More malloc, free Task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* _isNum - check if is a number
-*@num: string to check
-*Return: 1 is numm, 0 not num
-*/
+ * _isNum - check if is a number
+ * @num: string to check
+ * Return: 1 is numm, 0 not num
+ */
 int _isNum(char *num)
 {
 	int i;
@@ -20,12 +20,12 @@ int _isNum(char *num)
 }
 
 /**
-* *_memset - copies a character to the firstn characters of the string pointed
-*@s: original string
-*@b: value to remplace
-*@n: number of bytes
-*Return: s (string modify)
-*/
+ * *_memset - copies a character to the firstn characters of the string pointed
+ * @s: original string
+ * @b: value to remplace
+ * @n: number of bytes
+ * Return: s (string modify)
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -36,10 +36,10 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
-* _strlen - returns the lenght of a string
-*@s: poiter of character
-*Return: the length of a string
-*/
+ * _strlen - returns the lenght of a string
+ * @s: poiter of character
+ * Return: the length of a string
+ */
 int _strlen(char *s)
 {
 	int len;
@@ -51,11 +51,11 @@ int _strlen(char *s)
 }
 
 /**
-* main - multiple 2 positive numbers
-*@argc: argument counter
-*@argv: number to multiply
-*Return: 0 (success)
-*/
+ * main - multiple 2 positive numbers
+ * @argc: argument counter
+ * @argv: number to multiply
+ * Return: 0 (success)
+ */
 int main(int argc, char *argv[])
 {
 	int length, c, prod, i, j, l1, l2;
