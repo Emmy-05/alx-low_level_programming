1129f182596305c17b750a0cc3b517d738d96817
Emmy-05 <emekaokafor0715@gmail.com>
2022-10-11 09:50:49 -0700

    My 0x0E-structures_typedef Task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,15 @@
-#include <stdlib.h> include "dog.h"
+#include "dog.h"
+#include <stdlib.h>
 
-/** * free_dog - free memory *@d: array * Description: Free memory for struct */
-
+/**
+ * free_dog - frees memory held by passed struct
+ *
+ * @d: struct to free
+ * Return: nothing
+ */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
